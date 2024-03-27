fc98d797418e649b50dffec529745cdb4162229a
beth-codes <70582711+beth-codes@users.noreply.github.com>
2024-03-27 00:50:22 +0000

    Update Project.js

--- src/componenets/Project.js
+++ src/componenets/Project.js
@@ -19,7 +19,7 @@ function Project({ toggle }) {
               <span>Abbey House Hotel</span>
             </li>
             <li className="list-item" onClick={() => toggle("open1")}>
-              <span>Windbnb</span>
+              <span>Urbanvibes London</span>
             </li>
             <li className="list-item" onClick={() => toggle("open2")}>
               <span>Ray's Portfolio</span>
