f0064c514a153bc14bdc002bddd09ffd87fe1ef9
Elizabeth Adegunwa <70582711+beth-codes@users.noreply.github.com>
2024-10-29 07:40:42 +0000

    Update Project1.js

--- src/componenets/Project1.js
+++ src/componenets/Project1.js
@@ -7,7 +7,9 @@ function Project1() {
       </div>
       <div className="about-text">
         <p>
-          Welcome to the Employee Dashboard project, where seamless employee data management is at your fingertips. Built with Angular for a dynamic frontend experience, .NET for robust backend functionality, and CSS/SCSS for sleek design, this dashboard ensures a smooth and intuitive user experience. Hosted on Vercel for frontend deployment and Azure for backend deployment, you can effortlessly search for users, access detailed user information with a simple click, and seamlessly add and search for new users.
+          Exambly is a gamified exam prep platform I developed to improve student outcomes in Nigeria, featuring engaging practice tests, personalized learning with deep-learning algorithms, offline access, and a seamless, cross-platform experience to make exam preparation interactive and accessible.
+        <br/> 
+          Built using PHP, CMS, JavaScript (React, Node.js), SQL, HTML and CSS (SASS)
         </p>
         <h3 className="project-link">
           <a
